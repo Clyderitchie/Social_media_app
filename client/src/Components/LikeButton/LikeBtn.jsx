12c7af27aab38511b66db9c3d0f47a0c11902ddb
francis <clyderitchie@yahoo.com>
2024-03-10 17:31:33 -0700

    Like button is working. Shows total number of likes next to button

---
+++ client/src/Components/LikeButton/LikeBtn.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { useQuery } from '@apollo/client';
+import { QUERY_POST } from '../../utils/queries';
+import { LIKE_POST } from '../../utils/mutations';
+
+function LikeBtn({ postId, initialLikes }) {
+    const [likes, setLikes] = useState(initialLikes);
+    const [liked, setLiked] = useState(false);
+    const [likePost] = useMutation(LIKE_POST, {
+        variables: { postId },
+        onError: (error) => {
+            console.error('Error liking post:', error);
+        },
+        update: (cache, { data: { likePost } }) => {
+            const cachedPost = cache.readQuery({
+                query: QUERY_POST,
+                variables: { postId },
+            });
+            if (cachedPost) {
+                cache.writeQuery({
+                    query: QUERY_POST,
+                    variables: { postId },
+                    data: {
+                        getPost: {
+                            ...cachedPost.getPost,
+                            likes: likePost.likes,
+                        },
+                    },
+                });
+                setLikes(likePost.likes.length);
+                setLiked(true);
+            }
+        },
+    });
+
+    const handleLike = () => {
+        likePost();
+    };
+
+    return (
+        <>
+            <button className='btn btn-dark' onClick={handleLike} disabled={liked}>
+                Like
+            </button>
+            <span>{likes} Likes </span>
+        </>
+    )
+};
+
+export default LikeBtn;
