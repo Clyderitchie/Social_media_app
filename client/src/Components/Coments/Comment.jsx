22e54d0dd69b2976be3a82be5a4729c96cffca69
francis <clyderitchie@yahoo.com>
2024-03-20 18:21:35 -0700

    Comment text rendering user input

--- client/src/Components/Coments/Comment.jsx
+++ client/src/Components/Coments/Comment.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+
 import { useMutation, useQuery } from '@apollo/client';
+
 import { QUERY_POST } from '../../utils/queries';
 import { CREATE_COMMENT } from '../../utils/mutations';
 
@@ -9,8 +11,12 @@ function Comment({ postId }) {
     const [modalOpen, setModalOpen] = useState(false);
     const [post, setPost] = useState(null);
 
-    const { data } = useQuery(QUERY_POST, { variables: { postId }, fetchPolicy: 'cache-and-network' });
+    const [text, setText] = useState('');
 
+    const [addComment, { error }] = useMutation(CREATE_COMMENT);
+
+    const { data } = useQuery(QUERY_POST, { variables: { postId }, fetchPolicy: 'cache-and-network' });
+    
     const openModal = () => {
         if (data && data.getPost) {
             setPost(data.getPost);
@@ -22,6 +28,26 @@ function Comment({ postId }) {
         setModalOpen(false);
     };
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            if (!text.trim()) {
+                console.error('Comment text cannot be empty.');
+                return;
+            }
+
+            const { data } = await addComment({
+                variables: { text, postId }
+            });
+
+            setText('');
+            closeModal();
+        } catch (err) {
+            console.error('Error creating comment: ', err);
+        }
+    }
+
     return (
         <>
             <button type="button" onClick={openModal} className="btn btn-dark" data-bs-toggle='modal' data-bs-target='#staticBackdrop'>
@@ -29,28 +55,34 @@ function Comment({ postId }) {
             </button>
 
             {modalOpen && (
-                <div className="modal fade show" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true" style={{ display: 'block' }}>
-                    <div className="modal-dialog">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h1 className="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
-                                <button type="button" onClick={closeModal} className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div className="modal-body">
-                                {post && (
-                                    <div>
-                                        <h2>{post.title}</h2>
-                                        <p>{post.text}</p>
+                <form className="form" onSubmit={handleSubmit}>
+                    <div className="modal fade show" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true" style={{ display: 'block' }}>
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h1 className="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                                    <button type="button" onClick={closeModal} className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div className="modal-body">
+                                    {post && (
+                                        <div>
+                                            <h2>{post.title}</h2>
+                                            <p>{post.text}</p>
+                                        </div>
+                                    )}
+                                    <div className="d-flex justify-content-center">
+                                        <input type="text" className="form-control-lg rounded-pill" placeholder='Post' value={text} onChange={(e) => {
+                                            console.log('Input changed:', e.target.value)
+                                            setText(e.target.value)
+                                        }} />
                                     </div>
-                                )}
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="button" className="btn btn-primary">Post</button>
+                                    <button type="submit" className="btn btn-dark rounded-pill w-25">Post</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </form>
+
             )}
         </>
     )
