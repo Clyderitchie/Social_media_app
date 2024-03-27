902664c39d9caa1615ff94de88bd005f41195812
francis <clyderitchie@yahoo.com>
2024-03-27 04:25:20 -0700

    Deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,17 +1,5 @@
-// const mongoose = require('mongoose');
-
-
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetwork');
-
-// module.exports = mongoose.connection;
-
 const mongoose = require('mongoose');
 
-const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://clyderitchie:Tacomahc@5%405@socialmediaclone.9hbu1ko.mongodb.net/'; // Your MongoDB Atlas URI here
-
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetwork');
 
 module.exports = mongoose.connection;
