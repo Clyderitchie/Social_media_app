38e0b2e7a666d3100a8b16f3ee537db1339ce2a7
francis <clyderitchie@yahoo.com>
2024-03-27 14:41:26 -0700

    Trend section should be up and running" "

--- client/src/Components/PostContainer/SingleUserPost.jsx
+++ client/src/Components/PostContainer/SingleUserPost.jsx
@@ -5,20 +5,21 @@ import { QUERY_USER } from '../../utils/queries'
 
 import Auth from '../../utils/auth';
 import LikeBtn from '../LikeButton/LikeBtn';
+import Comment from '../Coments/Comment';
 
 function UserPost({ userId }) {
 
     // console.log('Single user post: ', userId);
 
     const { data } = useQuery(QUERY_USER, { variables: { userId }, fetchPolicy: 'cache-and-network' });
-   
+
     const user = data?.getUser || {};
 
     return (
         <>
-           {user.posts && user.posts.map((post) => (
-            <div className="card mb-4" key={post._id}>
-                <div className="card-body">
+            {user.posts && user.posts.map((post) => (
+                <div className="card mb-4" key={post._id}>
+                    <div className="card-body">
                         <div className="d-flex justify-content-start">
                             <h6 id="postCreator">@{post.userId.username}</h6>
                         </div>
@@ -33,12 +34,24 @@ function UserPost({ userId }) {
                         <div className="d-flex justify-content-start">
                             <p id="postCreatedAt">{post.createdAt}</p>
                         </div>
-                        <div className="d-flex justify-content-center">
-                            <LikeBtn postId={post._id} initialLikes={post.likes.length} />
+                        <div className="row">
+                            <div className="col-2 d-flex justify-content-start align-items-start">
+                                <div className=" ">
+                                    <div>
+                                        <LikeBtn postId={post._id} initialLikes={post.likes.length} />
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div className="col-10 d-flex justify-content-end align-items-start">
+                                <div className=''>
+                                    <Comment postId={post._id} />
+                                </div>
+                            </div>
                         </div>
                     </div>
-            </div>
-           ))}
+                </div>
+            ))}
 
         </>
     )
