687084ae3ab7c0a9b82a80f2a5e6225d74dbcdd0
francis <clyderitchie@yahoo.com>
2024-03-27 04:31:41 -0700

    Deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetwork');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://clyderitchie:Tacomahc%405@socialmediaclone.9hbu1ko.mongodb.net/');
 
 module.exports = mongoose.connection;
